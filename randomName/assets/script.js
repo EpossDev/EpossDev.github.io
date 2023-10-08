0c1ab0ef754956a7e96ad41fea6e2a745f6318a7
EpossDev <85056477+EpossDev@users.noreply.github.com>
2023-10-08 18:41:12 +0200

    Create script.js

---
+++ randomName/assets/script.js
@@ -0,0 +1,44 @@
+let names = [
+    'Cloé',
+    'Jéhanne',
+    'Jérémy',
+    'Lohan',
+    'Manon',
+    'Matis',
+    'Noah',
+    'Noéline',
+    'Sofia',
+    'Valentin',
+]
+
+function generateHTMLElement(name){
+    const elements = `
+    <div class="name-options"><input type="checkbox" id="name-${name}" name="name-${name}"><label for="name-${name}">${name}</label></div>
+    `
+    let container = document.querySelector('.name-options-container')
+    container.innerHTML += elements
+}
+function generateAllNames(){
+    names.forEach((item) => {
+        generateHTMLElement(item)
+    })
+}
+function randomNameFunction(){
+    let elements = document.querySelectorAll('.name-options')
+    let presentNames = []
+    elements.forEach((element) => {
+        if (element.firstElementChild.checked){
+            presentNames.push(element.lastElementChild.innerHTML)
+        }
+    })
+    let randomNumber = Math.floor(Math.random() * presentNames.length)
+    let randomName = presentNames[randomNumber]
+    let nameContainer = document.querySelector('.random-name-container')
+    if (randomName == null){
+        nameContainer.innerHTML = `<p style="color:#f00;">Merci de sélectionner des prénoms !</p>`;
+    }else {
+        nameContainer.innerHTML = `<p style="color:#32d74b;">${randomName}</p>`;
+
+    }
+}
+generateAllNames()
