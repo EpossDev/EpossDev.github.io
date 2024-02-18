03a4afeb7287b079eb4e38a6d1ce0890dd420588
EpossDev <85056477+EpossDev@users.noreply.github.com>
2024-02-18 17:47:01 +0100

    Update script.js

--- misterx/beta/assets/scripts/script.js
+++ misterx/beta/assets/scripts/script.js
@@ -1,12 +1,12 @@
 //V.2.0.0 Beta 1
 
-function currentTimeInMinutes(){
+function currentTimeInMinutes() {
 	let currentDate = new Date();
 	let hour = currentDate.getHours();
 	let minutes = currentDate.getMinutes();
 	return minutes+(hour*60);
 }
-function setSkyBackground(){
+function setSkyBackground() {
 	let t = currentTimeInMinutes();
 	document.querySelector('.sky-background').style.background = `var(--sky${Math.round(t/60)-1})`
 	document.body.style.backgroundColor = `var(--sky-main${Math.round(t/60)-1})`
