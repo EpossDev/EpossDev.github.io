02623e12b911c8d4c73564bf37fb7664a984bac9
EpossDev <85056477+EpossDev@users.noreply.github.com>
2024-02-25 16:46:59 +0100

    Update script.js

--- misterx/beta/assets/scripts/script.js
+++ misterx/beta/assets/scripts/script.js
@@ -2,13 +2,28 @@
 
 function currentTimeInMinutes() {
 	let currentDate = new Date();
-	let hour = currentDate.getHours();
-	let minutes = currentDate.getMinutes();
-	return minutes+(hour*60);
+	return currentDate.getMinutes()+(currentDate.getHours()*60);
 }
 function setSkyBackground() {
-	let t = currentTimeInMinutes();
-	document.querySelector('.sky-background').style.background = `var(--sky${Math.round(t/60)-1})`
-	document.body.style.backgroundColor = `var(--sky-main${Math.round(t/60)-1})`
+	document.querySelector('.sky-background').style.background = `var(--sky${Math.round(currentTimeInMinutes()/60)-1})`
+	document.body.style.backgroundColor = `var(--sky-main${Math.round(currentTimeInMinutes()/60)-1})`
 }
 setSkyBackground();
+
+
+document.querySelectorAll('.button-oc').forEach(function(i) {
+	i.addEventListener('click', function() {
+		document.getElementById('settings-container-main').classList.toggle('settings-container-main-close')
+		document.getElementById(`${event.target.id.split('-')[1]}`).classList.toggle('settings-container-open');
+	});
+});
+document.querySelectorAll('.button-toggle-container').forEach(function(i) {
+	i.addEventListener('click', function() {
+		event.target.classList.toggle('true');
+		if (event.target.classList.contains('button-toggle-container')) {
+			event.target.querySelector('.button-toggle').classList.toggle('true');
+		} else {
+			event.target.parentNode.classList.toggle('true');
+		}
+	});
+});
